de0262fc29c2bc8ee00e950752722aa32db1cbfd
Satyadeep Mishra <106036477+Sat-Star@users.noreply.github.com>
2023-10-18 14:46:48 +0530

    Update livescores.py

--- livescores.py
+++ livescores.py
@@ -47,32 +47,10 @@ def get_scores():
             away_wickets = 0
         print('\n')
         final= league + '\n' + home_team + ' ' + str(home_score) + '/' + str(home_wickets) + ' ( ' + str(home_over) + ' overs )\n' + away_team + ' ' + str(away_score) + '/' + str(away_wickets) + ' ( ' + str(away_over) + ' overs )'
-        #print(league , '\n' , home_team + ' ' , home_score , '/' , home_wickets , '(' , home_over , 'overs )\n' , away_team , ' ' , away_score , '/' , away_wickets , '(' , away_over , 'overs )' )
         finaly = finaly +'\n\n' + final
     return finaly
 
 
-# for i in jsondata['events']:
-#     league = i['tournament']['name']
-#     home_team = i['homeTeam']['name']
-#     away_team = i['awayTeam']['name']
-#     home_score = i['homeScore']['current']
-#     away_score = i['awayScore']['current']
-#     if 'innings' in i['homeScore']:
-#         home_over =i['homeScore']['innings']['inning1']['overs']
-#         home_wickets = i['homeScore']['innings']['inning1']['wickets']
-#     else:
-#         home_over = None
-#         home_wickets = None
 
-#     if 'innings' in i['awayScore']:
-#         away_over = i['awayScore']['innings']['inning1']['overs']
-#         away_wickets = i['awayScore']['innings']['inning1']['wickets']
-#     else:
-#         away_over = None
-#         away_wickets = None
-#     print('\n')
-#     final = league + '\n' + home_team + ' ' + str(home_score) + '/' + str(home_wickets) + ' ( ' + str(home_over) + ' overs )\n' + away_team + ' ' + str(away_score) + '/' + str(away_wickets) + ' ( ' + str(away_over) + ' overs )'
-#     print(final)
 
 
